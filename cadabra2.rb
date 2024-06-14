db6f7ddec32f1ae47aced16846b602d47f78c9e6
Kasper Peeters <kasper.peeters@phi-sci.com>
2024-06-14 20:53:20 +0100

    Add quotes.

--- cadabra2.rb
+++ cadabra2.rb
@@ -37,8 +37,8 @@ class Cadabra2 < Formula
   def install
     system "cmake", "-DPYTHON_SITE_PATH="+prefix+"/"+Language::Python.site_packages("python3.12"), "-DENABLE_MATHEMATICA=OFF", ".", *std_cmake_args
     venv = virtualenv_create(libexec)
-    venv.pip_install resource(sympy)
-    venv.pip_install resource(gmpy2)    
+    venv.pip_install resource("sympy")
+    venv.pip_install resource("gmpy2")    
     venv.pip_install_and_link buildpath
     system "make", "install" 
   end

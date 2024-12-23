6cfa5f318f626dd136932075256ef91ffdc106f9
Kasper Peeters <kasper.peeters@phi-sci.com>
2024-12-23 12:55:58 +0000

    Fix spacing.

--- cadabra2-devel.rb
+++ cadabra2-devel.rb
@@ -81,8 +81,8 @@ class Cadabra2Devel < Formula
   end
   
   resource "kiwisolver" do
-  url "https://files.pythonhosted.org/packages/85/4d/2255e1c76304cbd60b48cee302b66d1dde4468dc5b1160e4b7cb43778f2a/kiwisolver-1.4.7.tar.gz"
-  sha256 "9893ff81bd7107f7b685d3017cc6583daadb4fc26e4a888350df530e41980a60"
+    url "https://files.pythonhosted.org/packages/85/4d/2255e1c76304cbd60b48cee302b66d1dde4468dc5b1160e4b7cb43778f2a/kiwisolver-1.4.7.tar.gz"
+    sha256 "9893ff81bd7107f7b685d3017cc6583daadb4fc26e4a888350df530e41980a60"
   end
   
   resource "numpy" do

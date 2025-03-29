7795ba2f88893abfe021e5579fcdca3407d1cccf
Kasper Peeters <info@cadabra.science>
2025-03-29 19:52:02 +0000

    Update to release 2.5.11 commit d39446b8b9a2d0f8ea9383267e4739e2e68d7521.

--- cadabra2-devel.rb
+++ cadabra2-devel.rb
@@ -6,10 +6,10 @@ class Cadabra2Devel < Formula
   
   desc "A field-theory motivated approach to computer algebra (current development branch)."
   homepage "https://cadabra.science/"
-  url "https://github.com/kpeeters/cadabra2/archive/e7c7743240d469ff946846b6a203c87508557fd7.tar.gz"
-  sha256 "fd6b59b2e830a625cb3ac13239691fcc6667164bf5f6533e3300d517e36386a0"
+  url "https://github.com/kpeeters/cadabra2/archive/d39446b8b9a2d0f8ea9383267e4739e2e68d7521.tar.gz"
+  sha256 "1e16c28f80f40e59e97a597963f6e66d393f0c28054158118c9d2accc9e5d4a7"
   conflicts_with "cadabra2", because: "You can only install the normal or the devel version, not both."
-  version "2.5.11.e7c7743240d469ff946846b6a203c87508557fd7"
+  version "2.5.11.d39446b8b9a2d0f8ea9383267e4739e2e68d7521"
   
   depends_on "cmake" => :build
   depends_on "pkgconfig" => :build
